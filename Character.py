1bedb34a6d4f71e4030e480fe014b1fe83bce4bf
stanger <4blokadanhilla@gmail.com>
2020-12-18 17:52:29 +0300

    add some levels

--- Character.py
+++ Character.py
@@ -1,38 +1,30 @@
 from abc import ABC, abstractmethod
 from Items import *
 
+
 class Character(ABC):
 
     def __init__(self):
         self.current_health = 80
         self.max_health = 100
-        self.items = {"sword": None, "magic_book": None, "bow": None, "arrows": None}
+        self.items = {"Sword": None, "Magic_book": None, "Bow": None, "Arrows": None}
         self.totem = None
 
-    def attack(self, type_weapon):
+    def attack(self, type_weapon=None):
         arrow_damage = 0
-        if type_weapon == "bow":
-            arrow_damage = self.items['arrows'].get_damage()
-        return self.items[type_weapon].get_damage()+arrow_damage
+        if type_weapon == "Bow":
+            arrow_damage = self.items['Arrows'].get_damage()
+        return self.items[type_weapon].get_damage() + arrow_damage
 
     def take_weapon(self, weapon):
         if isinstance(weapon, Sword):
-            self.items['sword'] = weapon
+            self.items['Sword'] = weapon
         if isinstance(weapon, Bow):
-            self.items['bow'] = weapon
+            self.items['Bow'] = weapon
         if isinstance(weapon, Arrows):
-            self.items['arrows'] = weapon
+            self.items['Arrows'] = weapon
         if isinstance(weapon, MagicBook):
-            self.items['magic_book'] = weapon
-
-    # def print(self):
-    #     print("Здоровье персонажа " + str(self.current_health) + " из " + str(self.max_health))
-    #     print("В инвентаре есть: ")
-    #     for k, item in self.items.items():
-    #         if item is not None:
-    #             print(item)
-    #     if self.totem is not None:
-    #         print(self.totem)
+            self.items['Magic_book'] = weapon
 
     def get_inventory_string(self):
         stroka = "В инвентаре есть:\n"
@@ -44,38 +36,48 @@ class Character(ABC):
         return stroka
 
     def __str__(self):
-        return "Здоровье персонажа " + str(self.current_health) + " из "\
+        return "Здоровье персонажа " + str(self.current_health) + " из " \
                + str(self.max_health) + "\n" + self.get_inventory_string()
+
+    def get_available_weapons(self):
+        weapons = []
+        if self.items["Sword"] is not None:
+            weapons.append(self.items["Sword"])
+        if self.items["Magic_book"] is not None:
+            weapons.append(self.items["Magic_book"])
+        if self.items["Bow"] is not None and self.items["Arrows"] is not None:
+            weapons.append(self.items["Bow"])
+        return weapons
 
 
 class Wizard(Character):
-    def attack(self, type_weapon):
+    def attack(self, type_weapon="Magic_book"):
         attack_k = 1
-        if type_weapon == "magic_book":
+        if type_weapon == "Magic_book":
             attack_k *= 1.1
-        return super(Wizard, self).attack(type_weapon)*attack_k
+        return super(Wizard, self).attack(type_weapon) * attack_k
 
     def __str__(self):
         return "класс волшебник\n" + str(super(Wizard, self).__str__())
 
 
 class Archer(Character):
-    def attack(self, type_weapon):
+    def attack(self, type_weapon="Bow"):
         attack_k = 1
-        if type_weapon == "bow":
+        if type_weapon == "Bow":
             attack_k *= 1.1
-        return super(Archer, self).attack(type_weapon)*attack_k
+        return super(Archer, self).attack(type_weapon) * attack_k
 
     def __str__(self):
         return "класс лучник\n" + str(super(Archer, self).__str__())
 
 
 class Warrior(Character):
-    def attack(self, type_weapon):
+    def attack(self, type_weapon="Sword"):
         attack_k = 1
-        if type_weapon == "sword":
+        if type_weapon == "Sword":
             attack_k *= 1.1
-        return super(Warrior, self).attack(type_weapon)*attack_k
+        return super(Warrior, self).attack(type_weapon) * attack_k
 
     def __str__(self):
         return "класс воин\n" + str(super(Warrior, self).__str__())
@@ -97,7 +99,6 @@ class WizardFactory(CharacterFactory):
         else:
             wizard.take_weapon(weapon)
         return wizard
-
 
 
 class ArcherFactory(CharacterFactory):
--- Level.py
+++ Level.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from Items import *
 import random
 from Utils import *
+from Character import *
 
 
 class Level(ABC):
@@ -10,7 +11,9 @@ class Level(ABC):
     def play(self, hero, n) -> bool:
         pass
 
+
 class AppleLevel(Level):
+    """Класс уровня на котором герой съедает яблоко"""
 
     def play(self, hero, n) -> bool:
         apple = Apple()
@@ -22,27 +25,75 @@ class AppleLevel(Level):
         print("Герой съел яблоко и вылечился на " + str(apple.heal) + " здоровья")
         return True
 
+
 class PickUpLevel(Level):
+    """Класс уровня на котором герой может поднять предмет"""
     equipment_types = ["Sword", "Bow", "Arrows", "Magic_book"]
+
     def play(self, hero, n) -> bool:
         equipment_type = random.choice(self.equipment_types)
         equipment = None
         if equipment_type == "Sword":
-            equipment = Sword(random.randrange(3,20))
+            equipment = Sword(random.randrange(3, 20))
         if equipment_type == "Bow":
-            equipment = Bow(random.randrange(3,20))
+            equipment = Bow(random.randrange(3, 20))
         if equipment_type == "Arrows":
-            equipment = Arrows(random.randrange(3,20))
+            equipment = Arrows(random.randrange(3, 20))
         if equipment_type == "Magic_book":
-            equipment = MagicBook(random.randrange(3,20))
+            equipment = MagicBook(random.randrange(3, 20))
         print("Вы нашли предмет: ")
         print(equipment)
         print("1 - Подобрать предмет, 2 - Пройти мимо")
-        a = get_int_input([1,2])
+        a = get_int_input([1, 2])
         if a == 1:
             hero.take_weapon(equipment)
             print("Вы подобрали предмет " + str(equipment))
         return True
+
+
+class EnemyLevel(Level):
+    spawner_to_factory_mapping = {"Warrior": WarriorFactory(), "Archer": ArcherFactory(), "Wizard": WizardFactory()}
+    enemy_types = ["Warrior", "Archer", "Wizard"]
+
+    def play(self, hero, n) -> bool:
+        enemy_type = random.choice(self.enemy_types)
+        enemy = self.spawner_to_factory_mapping[enemy_type].create_character()
+        print("Вы встретили врага: \n" + str(enemy))
+        hero_step = True
+        while hero.current_health > 0 and enemy.current_health > 0:
+            self.step(hero, enemy, hero_step)
+            hero_step = not hero_step
+        return hero.current_health > 0
+
+    def step(self, hero, enemy, hero_step):
+        if hero_step:
+            enemy.current_health -= hero.attack(self.pick_available_weapon(hero))
+            print("Здоровье врага после удара героя " + str(enemy.current_health))
+        else:
+            hero.current_health -= enemy.attack() * self.get_defence(hero, enemy)
+            print("Здоровье героя после удара врага " + str(hero.current_health))
+
+    def pick_available_weapon(self, hero):
+        print("Каким оружием вы хотите атаковать врага?")
+        weapons = hero.get_available_weapons()
+        menu_items = []
+        for i in range(0, len(weapons)):
+            menu_items.append(i + 1)
+            print(str(i + 1) + " " + str(weapons[i]))
+        a = get_int_input(menu_items)
+        select_weapon = weapons[a - 1]
+        if isinstance(select_weapon, Sword):
+            return "Sword"
+        if isinstance(select_weapon, Bow):
+            return "Bow"
+        if isinstance(select_weapon, MagicBook):
+            return "Magic_book"
+
+    def get_defence(self, hero, enemy):
+        if type(hero) == type(enemy):
+            return 0.5
+        else:
+            return 1
 
 
 class LevelFactory(ABC):
@@ -51,11 +102,17 @@ class LevelFactory(ABC):
     def create_level(self):
         pass
 
+
 class AppleLevelFactory(LevelFactory):
     def create_level(self):
         return AppleLevel()
+
 
 class PickUpLevelFactory(LevelFactory):
     def create_level(self):
         return PickUpLevel()
 
+
+class EnemyLevelFactory(LevelFactory):
+    def create_level(self):
+        return EnemyLevel()
